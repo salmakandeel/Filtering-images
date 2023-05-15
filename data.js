75a6e45f4829f2e5aac6c289c5d8103892b50d59
salma kandeel <68400035+salmakandeel@users.noreply.github.com>
2023-05-15 09:49:33 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,16 @@
+
+export const images=[
+    {"name":"logo all","src":"images/logo.png"},
+    {"name":"logo all","src":"images/logo4.png"},
+    {"name":"logo all","src":"images/logo2.png"},
+    {"name":"logo all","src":"images/logo3.jpg"},
+    {"name":"mobileApp all ","src":"images/mob.jpg"},
+    {"name":"mobileApp all","src":"images/mob1.jpg"},
+    {"name":"mobileApp all","src":"images/mob3.jpg"},
+    {"name":"mobileApp all","src":"images/mob2.png"},
+    {"name":"webDesign all","src":"images/download.jpg"},
+    {"name":"webDesign all","src":"images/download (3).jpg"},
+    {"name":"webDesign all","src":"images/download (2).jpg"},
+    {"name":"webDesign all","src":"images/download (1).jpg"}
+
+]
---
+++ script.js
@@ -0,0 +1,46 @@
+
+import { images } from './data.js'
+// const containerDiv=document.getElementsByClassName("container")
+console.log(images);
+function getAllImages(){
+const button=document.querySelector(".active")
+   button? button.classList.remove('active'):null
+    document.getElementById('all').classList.add('active')
+    document.getElementById("Id").innerHTML=""
+    images.map(image=>{
+    var img=document.createElement("img");
+    img.src=image.src  
+    document.getElementById("Id").appendChild(img);
+       
+    })
+}
+const displayAll=document.getElementById('all')
+displayAll.addEventListener('click',function(){getspesficImages('all')})
+
+function getspesficImages(imgType){
+    const button=document.querySelector(".active")
+    button? button.classList.remove('active'):null
+    document.getElementById(imgType).classList.add('active')
+    document.getElementById("Id").innerHTML=""
+    images.filter(image=>{
+        if(image.name.indexOf(imgType) > -1 )
+    {
+    var img=document.createElement("img")
+    img.src=image.src  
+    document.getElementById("Id").appendChild(img)}
+       
+    })
+}
+
+
+
+let displaySpecficImagesButton=document.getElementById('webDesign')
+displaySpecficImagesButton.addEventListener('click',function(){getspesficImages('webDesign')})
+
+
+
+ displaySpecficImagesButton=document.getElementById('mobileApp')
+displaySpecficImagesButton.addEventListener('click',function(){getspesficImages('mobileApp')})
+
+ displaySpecficImagesButton=document.getElementById('logo')
+ displaySpecficImagesButton.addEventListener('click',function(){getspesficImages('logo')})
